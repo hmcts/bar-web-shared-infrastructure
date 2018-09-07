f8c89cc025a865c083ff57430fd49fa22f55939b
jaleen <jalal.din@gmail.com>
2018-09-07 16:23:43 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -32,3 +32,20 @@ variable "tenant_id" {
 variable "jenkins_AAD_objectId" {
   description = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
 }
+
+  variable "team_name" {
+  default = "BAR"
+}
+
+variable "team_contact" {
+  default = "#bar-tech"
+}
+
+variable "name" {
+  default = false
+}
+
+variable "asp_capacity" {
+  default = 2
+}
+

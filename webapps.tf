023825a10c8c9ff0b42a82b5963a89e067a67538
jaleen <jalal.din@gmail.com>
2018-10-03 13:02:14 +0100

    Update webapps.tf

--- webapps.tf
+++ webapps.tf
@@ -3,7 +3,7 @@ module "appServicePlanA" {
   source              = "git@github.com:hmcts/cnp-module-app-service-plan?ref=master"
   location            = "UK South"
   env                 = "${var.env}"
-  resource_group_name = "${azurerm_resource_group.shared_resource_group.name}"
+  resource_group_name = "${var.product}-${var.env_type}-rg"
   asp_capacity        = "${var.asp_capacity}"
   asp_name            = "${var.product}"
   ase_name            = "${local.ase_name}"

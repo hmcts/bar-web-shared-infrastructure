c41d698a258b03dc3935d50f9e7c3c81a59fbecc
jaleen <jalal.din@gmail.com>
2018-10-03 15:22:09 +0100

    Update webapps.tf

--- webapps.tf
+++ webapps.tf
@@ -3,7 +3,7 @@ module "appServicePlanA" {
   source              = "git@github.com:hmcts/cnp-module-app-service-plan?ref=master"
   location            = "UK South"
   env                 = "${var.env}"
-  resource_group_name = "${var.product}-${var.env_type}-rg"
+  resource_group_name = "${var.product}-${var.env}-rg"
   asp_capacity        = "${var.asp_capacity}"
   asp_name            = "${var.product}-asp"
   ase_name            = "${local.ase_name}"

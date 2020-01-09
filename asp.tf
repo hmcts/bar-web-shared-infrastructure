cebb0dea38fe8e4712c816eef4ca44191270ed38
Teo <35260759+tpetkovic@users.noreply.github.com>
2020-01-09 13:52:53 +0000

    Update asp.tf

--- asp.tf
+++ asp.tf
@@ -2,7 +2,7 @@ locals {
   sku_size = "${var.env == "prod" || var.env == "aat" ? "I2" : "I1"}"
 }
 module "appServicePlan" {
-  source  = "git@github.com:hmcts/cnp-module-app-service-plan?ref=OUTPUT-TEMPLATE-DEPLOYS-USING-LOOKUP"
+  source  = "git@github.com:hmcts/cnp-module-app-service-plan?ref=master"
   location = "${var.location}"
   env = "${var.env}"
   resource_group_name = "bar-${var.env}-rg"

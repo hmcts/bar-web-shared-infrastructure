28e0148b4fccbbc8f911e3afe00912dcf2d687e0
Anil Dhawale <anil.dhawale@hmcts.net>
2020-10-21 16:47:28 +0100

    build issue fix

--- application-insights.tf
+++ application-insights.tf
@@ -1,5 +1,5 @@
 resource "azurerm_application_insights" "appinsights" {
-  name                = join("-", [var.product, var.env])
+  name                = "${var.product}-${var.env}"
   location            = var.appinsights_location
   resource_group_name = azurerm_resource_group.rg.name
   application_type    = var.application_type
